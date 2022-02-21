b007ae3e9b996c205e91fa7c0c0be60640b4b211
g4eng <gaeng1107@gmail.com>
2022-02-21 22:28:41 +0900

    크롤링 로직 수정중

--- Crawling/crawling.py
+++ Crawling/crawling.py
@@ -11,25 +11,39 @@ def fmKorea():
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
         # select 정의하기
-        title_elem = soup.select('li.li.li_best2_pop0 > div.li > h3.title > a.hotdeal_var8')
-        dealInfo_elem = soup.select('li.li.li_best2_pop0 > div.li > div.hotdeal_info')
-        count = len(title_elem)
-        # 제목 가져오기
+        itemLists = soup.select('li.li.li_best2_pop0')
+
+        thumbnails = []
         titles = []
-        for title in title_elem:
-            text = title.text.split('[')[0]
-            titles.append(text.strip())
-        # 핫딜 정보 가져오기
-        dealInfos = []
-        for deal in dealInfo_elem:
-            dealInfo = deal.text.split('/')
-            mall = dealInfo[0].strip().split(':')[1].strip()
-            itemPrice = dealInfo[1].strip().split(':')[1].strip()
-            shipPrice = dealInfo[2].strip().split(':')[1].strip()
-            dealInfos.append((mall, itemPrice, shipPrice))
-        for i in range(0, count):
-            #print(titles[i], dealInfos[i])
-            titles
+        itemPrices = []
+        shippingPrices = []
+        urls = []
+
+        for item in itemLists:
+            thumbnail = item.find('img')
+            print(thumbnail)
+            
+
+        # title_elem = soup.select('li.li.li_best2_pop0 > div.li > h3.title > a.hotdeal_var8')
+        # dealInfo_elem = soup.select('li.li.li_best2_pop0 > div.li > div.hotdeal_info')
+        # thumbnail_elem = soup.select('img.class')
+        # count = len(title_elem)
+        # # 제목 가져오기
+        # titles = []
+        # for title in title_elem:
+        #     text = title.text.split('[')[0]
+        #     titles.append(text.strip())
+        # # 핫딜 정보 가져오기
+        # dealInfos = []
+        # for deal in dealInfo_elem:
+        #     dealInfo = deal.text.split('/')
+        #     mall = dealInfo[0].strip().split(':')[1].strip()
+        #     itemPrice = dealInfo[1].strip().split(':')[1].strip()
+        #     shipPrice = dealInfo[2].strip().split(':')[1].strip()
+        #     dealInfos.append((mall, itemPrice, shipPrice))
+        # for i in range(0, count):
+        #     #print(titles[i], dealInfos[i])
+        #     titles
     else:
         print(response.status_code)
 
@@ -40,8 +54,11 @@ def ppomppu():
         soup = BeautifulSoup(html, 'html.parser')
         dealInfos_elem = soup.select('div > a > font.list_title')
         dealInfos = []
+        titles = []
+        malls = []
+        itemPrices = []
         for dealInfo in dealInfos_elem:
             print(dealInfo.text)
 
 fmKorea()
-ppomppu()
+#ppomppu()
