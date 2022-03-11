2006ea3ae51b390789c080834fac650b3040dc33
Yang Kyeong Mo(Kyle) <gaeng1107@gmail.com>
2022-03-11 21:31:14 +0900

    Update storeFirebase.py

--- Crawling/storeFirebase.py
+++ Crawling/storeFirebase.py
@@ -2,7 +2,6 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-
 def setup():
     cred = credentials.Certificate("fashion-hotdeal-firebase-adminsdk-4j25u-1669072ff8.json")
     firebase_admin.initialize_app(cred, {
